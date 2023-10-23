7f1d86ecc976289b94b0cf4d6d14c472fd58dee5
David Espino <davidespinog@gmail.com>
2023-10-23 11:13:37 -0600

    ref

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
-<script setup>
-  import { ref } from 'vue';
-  import Menu from './components/Menu.vue';
-  const show = ref(true);
-</script>
 
 <template>
-  <button @click="show = !show">Menú</button>
-  <transition name="fade">
-    <Menu v-show="show"/>
-  </transition>
+  <Home />
+
 </template>
 
-<style scoped>
+<script>
+import Home from './components/Home.vue';
 
-.fade-enter-from {
-  opacity: 0;
+export default {
+  name: 'App', 
+  components: {
+    Home,
+  }
 }
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-leave-active, .fade-enter-active {
-  transition: opacity 0.5s ease;
-}
+</script>
 
-</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Home</div>
+  <div> {{ text }}</div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const text = ref('Hola Vue');
+    return {
+      text,
+    }
+  }
+}
+</script>
